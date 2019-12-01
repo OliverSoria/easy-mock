7ca8ac0fe0ebeae855c3663ce075cbc8787fed7a
Oliver Soria <oliversoria@hotmail.com>
2019-12-01 04:14:43 -0600

    Update: se formatea código

--- src/main/java/com/easy/mock/controller/rest/JsonController.java
+++ src/main/java/com/easy/mock/controller/rest/JsonController.java
@@ -6,7 +6,10 @@ import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
@@ -43,7 +46,7 @@ public class JsonController {
     }
 
     @PostMapping("clear")
-    public ResponseEntity<Object>clear() {
+    public ResponseEntity<Object> clear() {
         this.model.setValue("{}");
         return new ResponseEntity<>(HttpStatus.OK);
     }
